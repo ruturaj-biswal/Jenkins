0837a8d27e11b63b3c08105771cd6ba3370d3697
git-demo <ruturajbiswal18@gmail.com>
2026-02-07 16:08:10 +0530

    Fix Java compilation error

--- simpleWebApp/src/main/java/com/ruturaj/simpleWebApp/SimpleWebAppApplication.java
+++ simpleWebApp/src/main/java/com/ruturaj/simpleWebApp/SimpleWebAppApplication.java
@@ -1,8 +1,8 @@
 package com.ruturaj.simpleWebApp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication
-        
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SimpleWebAppApplication {
 
